d60324d67eb3b15fc67f3a626fdd25359213516b
Jonathan <jonathanthornton1997@gmail.com>
2020-09-17 15:06:51 -0700

    MVP

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,22 +2,41 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# def moving_zeroes(arr):
+#     # Your code here
+#     left =[]
+#     right = []
+
+#     # O(n)
+#     for x in arr:
+#         if x == 0:
+#             right.append(x)
+#         else:
+#             left.append(x)
+   
+#     arr = left + right
+#     # print(arr)
+#     return arr
+  
+            
 def moving_zeroes(arr):
-    # Your code here
-    left =[]
-    right = []
+    left = 0
+    right = len(arr) -1
 
-    for x in arr:
-        if x == 0:
-            right.append(x)
+    while left< right:
+        if arr[left] == 0:
+            if arr[right] != 0:
+                arr[left], arr[right] = arr[right], arr[left]
+                right -= 1
+                left += 1
+            else:
+                right -= 1
         else:
-            left.append(x)
-
-    print(f"This is the left list {left}")    
-    print(f"This is the right list {right}")    
-    print(left.extend(right))
-    # print(arr)
+            left += 1
     return arr
+
+        
+
 
 
     
@@ -25,6 +44,6 @@ def moving_zeroes(arr):
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr = [1, 2, 3, 0, 4, 0, 0,]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
